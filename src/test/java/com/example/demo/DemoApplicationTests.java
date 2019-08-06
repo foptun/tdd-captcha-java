e5b1ba7becbd24ca38b240f246cb1b23a13c3a38
foptun <oixuier.toppfun@gmail.com>
2019-08-06 17:13:35 +0700

    Commit Test Fail

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.captcha.Chapcha;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -12,12 +10,4 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-    @Test
-    public void operatorPattern_1_ShouldBe_Multiply(){
-        Chapcha chapcha = new Chapcha(1,1,3,1);
-        String operator = chapcha.getOperator();
-
-        assertEquals("asdasdasdasdasdas", operator);
-    }
 }
